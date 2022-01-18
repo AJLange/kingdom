0ab91f0f326e5fd471f803fb3d789e345ee5a86f
Amanda Lange <litagemini@gmail.com>
2022-01-18 13:55:51 -0500

    stubbing out some files

---
+++ commands/cmdsets/charinfo.py
@@ -0,0 +1,17 @@
+"""
+Commands related to getting information about characters.
+
+To start, needs
+
+Finger
+OOCfinger
+Personal news/info
+FClist
+NPClist
+Commands related to FC sorting
+
+Other fun info options as needed
+"""
+
+
+from evennia import CmdSet
---
+++ commands/cmdsets/movement.py
@@ -0,0 +1,8 @@
+"""
+All the stuff related to moving around the grid, such as 
+teleport, follow, summon, will go in here.
+
+"""
+
+
+from evennia import CmdSet
