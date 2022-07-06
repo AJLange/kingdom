7d9d78153e45dbb05e48ddca9689c5c70772b689
Amanda Lange <litagemini@gmail.com>
2022-07-05 20:03:44 -0400

    reconfiguring multidesc

--- commands/cmdsets/descer.py
+++ commands/cmdsets/descer.py
@@ -9,7 +9,10 @@ from evennia.contrib import multidescer
 from evennia.objects.models import ObjectDB
 from evennia import CmdSet
 from evennia import default_cmds
+from evennia.commands.default.muxcommand import MuxCommand
 
+#permissions on desc are strange. 
+#todo, allow PCs to desc themselves, but not rooms.
 
 class CmdDesc(default_cmds.CharacterCmdSet):
     """
@@ -67,6 +70,79 @@ class CmdDesc(default_cmds.CharacterCmdSet):
 
         if "edit" in self.switches:
             self.edit_handler()
+            return
+
+        if "=" in self.args:
+            # We have an =
+            obj = caller.search(self.lhs)
+            if not obj:
+                return
+            desc = self.rhs or ""
+        else:
+            obj = caller.location or self.msg("|rYou can't describe oblivion.|n")
+            if not obj:
+                return
+            desc = self.args
+        if obj.access(self.caller, "control") or obj.access(self.caller, "edit"):
+            obj.db.desc = desc
+            caller.msg("The description was set on %s." % obj.get_display_name(caller))
+        else:
+            caller.msg("You don't have permission to edit the description of %s." % obj.key)
+
+
+class CmdMultiDesc(MuxCommand):
+    """
+    +multidesc = <text of desc>
+    +mdesc <text of desc>
+    +multidesc/store <name>
+    +multidesc/wear <name>
+    +multidesc/view <name>
+    +multidesc/list
+    
+    Use the multidescer to describe yourself. 
+    Multidesc, by itself, lists your current descs, or use +mutidesc/list
+    for the same functionality.
+
+    +multidesc/store stores your current desc under the name you
+    have provided.
+
+    +multidesc/wear will put on a desc you've already stored.
+
+    +multidesc/view shows the desc before you wear it.
+
+    +mdesc is an alias for +multidesc and does the same thing.
+
+    this doesn't work yet I'm just typing out this stuff.
+
+    """
+
+    key = "multidesc"
+    aliases = "+multidesc, mdesc, +mdesc"
+    switch_options = ("list","store","wear","view")
+    help_category = "Building"
+
+    def func(self):
+        """Define command"""
+
+        caller = self.caller
+        if not self.args and "edit" not in self.switches:
+            caller.msg("Usage: desc [<obj> =] <description>")
+            return
+
+        if "list" in self.switches:
+            
+            return
+
+        if "store" in self.switches:
+            
+            return
+
+        if "wear" in self.switches:
+            
+            return
+
+        if "view" in self.switches:
+            
             return
 
         if "=" in self.args:
