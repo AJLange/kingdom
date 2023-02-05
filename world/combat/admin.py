45eee3192899077595fd0bf4f618bde966702219
Amanda Lange <litagemini@gmail.com>
2023-02-05 17:47:02 -0500

    checking in combat models

--- world/combat/admin.py
+++ world/combat/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from world.combat.models import Weapon, ArmorMode, BusterList
+from world.combat.models import Weapon, Armor, BusterList, GenericAttack
 
 class WeaponAdmin(admin.ModelAdmin):
     list_display = ("id", "db_name","db_class","db_type_1", "db_type_2", "db_type_3")
@@ -10,11 +10,10 @@ class WeaponAdmin(admin.ModelAdmin):
     save_on_top = True
     list_select_related = True
 
-class ArmorModeAdmin(admin.ModelAdmin):
+class ArmorAdmin(admin.ModelAdmin):
     list_display = ("id", "db_name",)
-    list_display_links  = ("id","db_name")
+    list_display_links  = ("id","db_name",)
     
-    search_fields = ["^db_date_created", "^db_name"]
     save_as = True
     save_on_top = True
     list_select_related = True
@@ -23,12 +22,25 @@ class BusterListAdmin(admin.ModelAdmin):
     list_display = ("id","db_thief", "db_name", "db_class","db_type_1", "db_type_2", "db_type_3")
     list_display_links  = ("id","db_thief","db_name")
     
-    search_fields = ["^db_date_created", "^db_title"]
+    search_fields = ["^db_date_created", "^db_thief", "^db_name"]
+    save_as = True
+    save_on_top = True
+    list_select_related = True
+
+class GenericAttackAdmin(admin.ModelAdmin):
+    list_display = ("id","db_name", "db_class")
+    list_display_links  = ("id","db_name")
+    
+    search_fields = ["^db_date_created"]
     save_as = True
     save_on_top = True
     list_select_related = True
 
 
+
 admin.site.register(Weapon, WeaponAdmin)
+admin.site.register(Armor, ArmorAdmin)
 admin.site.register(BusterList, BusterListAdmin)
+admin.site.register(GenericAttack, GenericAttackAdmin)
+
 
--- world/combat/models.py
+++ world/combat/models.py
@@ -1,8 +1,12 @@
 from django.db import models
 from evennia.utils.idmapper.models import SharedMemoryModel
+from django.test.utils import override_settings
 
 
-
+'''
+WeaponClasses determine which roll of dice is used to hit 
+with this attack.
+'''
 
 class WeaponClass(models.IntegerField):
         RANGED = 1
@@ -189,15 +193,91 @@ class Weapon(SharedMemoryModel):
     db_date_created = models.DateTimeField('date created', editable=False,
                                             auto_now_add=True, db_index=True)
 
+'''
+Mode Swap messages:
 
+Mode: '<player> has activated <pronoun>'s <name> mode!'
+Stance: '<player> has swapped to <pronoun>'s <name> stance!'
+Focus: '<player> focuses <pronoun>'s efforts, becoming <name>!'
+Form: '<player> changes forms, becoming <pronoun>'s <name>!'
+VR: '<player> jacks in, activating <name>!'
+Summon: '<player> summons <name> to assist!'
+Minion: '<player> is playing as squadron <name>.'
+System: '<player> activates <pronoun>'s <name> system!'
+Armor: '<player>' activates <pronoun>'s <name> armor!'
+'''
 
-class ArmorMode(SharedMemoryModel):
+class Armor(SharedMemoryModel):
     # armor mode object for holding stats
-    class Meta:
-        managed = False
+
+    db_name = models.CharField('Name', max_length=120)
 
     db_date_created = models.DateTimeField('date created', editable=False,
                                             auto_now_add=True, db_index=True)
+
+    class ModeSwap(models.IntegerField):
+        MODE = 1
+        STANCE = 2
+        FOCUS = 3
+        FORM = 4
+        VR = 5
+        SUMMON = 6
+        MINION = 7
+        SYSTEM = 6
+        ARMOR = 7
+        TYPE_CHOICES = (
+            (MODE, 'Mode'),
+            (STANCE, 'Stance'),
+            (FOCUS, 'Focus'),
+            (FORM, 'Form'),
+            (VR, 'VR'),
+            (SUMMON, 'Summon'),
+            (MINION, 'Minion'),
+            (SYSTEM, 'System'),
+            (ARMOR, 'Armor')
+        )
+    db_swap = models.IntegerField('Swap Style',
+        choices=ModeSwap.TYPE_CHOICES, default=1
+    )
+    
+    #stats   
+    
+    db_pow = models.IntegerField('Power', default=1)
+    db_dex = models.IntegerField('Dexterity', default=1)
+    db_ten = models.IntegerField('Tenacity', default=1)
+    db_cun = models.IntegerField('Cunning', default=1)
+    db_edu = models.IntegerField('Education', default=1)
+    db_chr = models.IntegerField('Charisma', default=1)
+    db_aur = models.IntegerField('Aura', default=1)
+
+    db_size = models.CharField('Size',blank=True,null=True, max_length=20)
+    db_speed = models.IntegerField('Speed',default=1)
+    db_strength = models.CharField('Strength',blank=True,null=True, max_length=30)
+
+    db_resistance = models.CharField('Resistance',blank=True,null=True, max_length=100)
+    db_weakness = models.CharField('Weakness',blank=True,null=True, max_length=100)
+    
+    #abilities    
+
+    db_discern = models.IntegerField('Discern', default=1)
+    db_aim = models.IntegerField('Aim', default=1)
+    db_athletics = models.IntegerField('Athletics', default=1)
+    db_force =  models.IntegerField('Force', default=1)
+    db_mechanics = models.IntegerField('Mechanics', default=1)
+    db_medicine =  models.IntegerField('Medicine', default=1)
+    db_computer = models.IntegerField('Computer', default=1)
+    db_stealth = models.IntegerField('Stealth', default=1) 
+    db_heist = models.IntegerField('Heist', default=1)
+    db_convince = models.IntegerField('Convince', default=1)
+    db_presence = models.IntegerField('Presence', default=1)
+    db_arcana = models.IntegerField('Arcana', default=1)
+
+    # capabilities as string match for now. structure this data later.
+    
+    db_capabilities = models.TextField('Capabilities',blank=True,null=True)
+
+    def __str__(self):
+        return self.db_name
 
 class BusterList(SharedMemoryModel):
     '''
@@ -242,3 +322,19 @@ class BusterList(SharedMemoryModel):
                                             auto_now_add=True, db_index=True)
     #todo: autopopulate this. fine for now.
     db_time_out = models.DateTimeField('time out',blank=True, null=True)
+
+
+class GenericAttack(SharedMemoryModel):
+
+    '''
+    Generic moves are available to all players.
+    They are 'weaponless' and do not have a type.
+
+    Basic Melee, Basic Grapple, Thrown Object
+    '''
+
+    db_name = models.CharField('Name', max_length=200)
+
+    db_class = models.IntegerField(
+        choices=WeaponClass.TYPE_CHOICES
+    )
